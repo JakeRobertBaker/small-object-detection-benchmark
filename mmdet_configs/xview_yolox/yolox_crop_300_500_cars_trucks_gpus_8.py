1f106ed683d6f262de8b47b87e0327050b032414
JakeRobertBaker <bakej159.302@gmail.com>
2022-07-07 14:27:28 +0100

    tidies configs

--- mmdet_configs/xview_yolox/yolox_crop_300_500_cars_trucks_gpus_8.py
+++ mmdet_configs/xview_yolox/yolox_crop_300_500_cars_trucks_gpus_8.py
@@ -1,13 +1,14 @@
-_base_ = ["../tood/tood_r50_fpn_1x_coco.py"]
+_base_ = ["../yolox/yolox_s_8x8_300e_coco_pipeline.py"]
 
 
-EXP_NAME = "tood_crop_300_500_cls_cars_trucks"
+EXP_NAME = "yolox_crop_300_500_cars_trucks"
 DATA_ROOT = "data/xview/"
-BATCH_MULTIPLIER = 2
+BATCH_MULTIPLIER = 8
 LR_MULTIPLIER = 1
 EVAL_INTERVAL = 3
+NUM_CLASSES = 60
 DATASET_REPEAT = 50
-TAGS = ["tood", "crop=300_500", "24epochs", f"num_cls={NUM_CLASSES}", f"repeat={DATASET_REPEAT}"]
+TAGS = ["yolox", "crop=300_500", "24epochs", f"num_cls={NUM_CLASSES}", f"repeat={DATASET_REPEAT}"]
 CLASSES = (
     "Small Car",
     "Bus",
@@ -27,41 +28,27 @@ NUM_CLASSES = len(CLASSES)
 # model settings
 model = dict(
     bbox_head=dict(
+        type='YOLOXHead',
         num_classes=NUM_CLASSES,
     ),
 )
 
 # dataset settings
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
 train_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(type="LoadAnnotations", with_bbox=True),
-    dict(
-        type="AutoAugment",
-        policies=[
-            [
-                dict(type="RandomCrop", crop_type="absolute_range", crop_size=(300, 500), allow_negative_crop=True),
-                dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
-            ],
-            [
-                dict(type="RandomCrop", crop_type="absolute_range", crop_size=(300, 500), allow_negative_crop=True),
-                dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
-            ],
-            [
-                dict(type="RandomCrop", crop_type="absolute_range", crop_size=(300, 500), allow_negative_crop=True),
-                dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
-            ],
-            [
-                dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
-            ],
-        ],
-    ),
+    dict(type="RandomCrop", crop_type="absolute_range", crop_size=(300, 500), allow_negative_crop=True),
+    dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
     dict(type="RandomFlip", flip_ratio=0.5),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="Pad", size_divisor=32),
     dict(type="DefaultFormatBundle"),
     dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
 ]
+
+
 test_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(
@@ -73,7 +60,7 @@ test_pipeline = [
             dict(type="RandomFlip"),
             dict(type="Normalize", **img_norm_cfg),
             dict(type="Pad", size_divisor=32),
-            dict(type="ImageToTensor", keys=["img"]),
+            dict(type="DefaultFormatBundle"),
             dict(type="Collect", keys=["img"]),
         ],
     ),
@@ -110,13 +97,27 @@ data = dict(
 # optimizer
 # default 8 gpu
 # /8 for 1 gpu
-optimizer = dict(lr=0.001 / 8 * BATCH_MULTIPLIER * LR_MULTIPLIER, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(
+    lr=0.01 / 8 * BATCH_MULTIPLIER * LR_MULTIPLIER, paramwise_cfg=dict(bias_lr_mult=2.0, bias_decay_mult=0.0)
+)
 
 checkpoint_config = dict(interval=1, max_keep_ckpts=1, save_optimizer=False)
 evaluation = dict(interval=EVAL_INTERVAL, metric="bbox", save_best="auto")
 
 # learning policy
-lr_config = dict(policy="step", warmup="linear", warmup_iters=500 * 8 / BATCH_MULTIPLIER, warmup_ratio=0.001, step=[16, 22])
+# lr_config = dict(policy="step", warmup="linear", warmup_iters=500, warmup_ratio=0.1, step=[16, 22])
+
+lr_config = dict(
+    _delete_=True,
+    policy='YOLOX',
+    warmup='exp',
+    by_epoch=False,
+    warmup_by_epoch=True,
+    warmup_ratio=1,
+    warmup_iters=5,  # 5 epoch
+    num_last_epochs=10,
+    min_lr_ratio=0.05)
+
 runner = dict(type="EpochBasedRunner", max_epochs=24)
 
 # logger settings
@@ -128,5 +129,5 @@ log_config = dict(
     ],
 )
 
-load_from = "https://download.openmmlab.com/mmdetection/v2.0/tood/tood_r50_fpn_1x_coco/tood_r50_fpn_1x_coco_20211210_103425-20e20746.pth"
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_s_8x8_300e_coco/yolox_s_8x8_300e_coco_20211121_095711-4592a793.pth"
 work_dir = f"runs/xview/{EXP_NAME}/"
